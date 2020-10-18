28a94541819f16a1c632ffd5f36d89c8aefdae84
Patryk Gułaś <patryk.gulas@digifit.pl>
2020-10-18 16:37:10 +0200

    feat: about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,62 @@
 <template>
-  <div class="container mx-auto">__ABOUT</div>
+  <div class="container mx-auto">
+    <div class="flex items-center">
+      <img
+        class="w-32 h-32 rounded-full mr-8 border-2 border-gray-300 p-1"
+        src="~/assets/photo.jpg"
+      />
+      <div>
+        <img class="h-16" src="~/assets/waving_hand.gif" />
+        <div class="mt-4 text-xl">
+          Hi, I'm Patrick and I'm a fullstack web engineer living in Poland but
+          working remotely for international clients (i.a. Microsoft, Aviva,
+          Asseco and Predica). I was a member of small startup teams and big
+          corporate projects. Here you can find some information about my
+          developer profile.
+        </div>
+        <div class="mt-4 font-semibold">
+          <a
+            class="text-gray-700 hover:text-gray-900"
+            href="https://twitter.com/ignarweb"
+            target="_blank"
+            >Twitter</a
+          >
+          <span>⋅</span>
+          <a
+            class="text-gray-700 hover:text-gray-900"
+            href="https://github.com/pgvlas"
+            target="_blank"
+            >Github</a
+          >
+          <span>⋅</span>
+          <a
+            class="text-gray-700 hover:text-gray-900"
+            href="https://www.linkedin.com/in/patryk-gu%C5%82a%C5%9B-86ab939a/"
+            target="_blank"
+            >Linkedin</a
+          >
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="mt-16">
+        <h1 class="text-2xl font-bold">Technologies I worked with</h1>
+        <div class="flex items-center mt-8 flex-wrap">
+          <HtmlLogo class="h-12 w-auto mr-12 mb-12" />
+          <CssLogo class="h-12 w-auto mr-12 mb-12" />
+          <JavascriptLogo class="h-12 w-auto mr-12 mb-12" />
+          <TypescriptLogo class="h-12 w-auto mr-12 mb-12" />
+          <ReactLogo class="h-12 w-auto mr-12 mb-12" />
+          <AngularLogo class="h-12 w-auto mr-12 mb-12" />
+          <VueLogo class="h-12 w-auto mr-12 mb-12" />
+          <NuxtLogo class="h-12 w-auto mr-12 mb-12" />
+          <NodeLogo class="h-12 w-auto mr-12 mb-12" />
+          <MongoLogo class="h-10 w-auto mr-12 mb-12" />
+          <ExpressLogo class="h-8 w-auto mr-12 mb-12" />
+          <AwsLogo class="h-10 w-auto mr-12 mb-12" />
+          <AzureLogo class="h-8 w-auto mr-12 mb-12" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -10,15 +10,17 @@
       </h1>
       <HelloIllustration class="w-auto h-64 mt-16" />
     </div>
-    <div class="mt-16 text-xl">
-      I'm fullstack web engineer living in the central Europe. I'm taking part
-      in web development journey for many years and I can see how web is
-      transforming. I mostly work with stable Javascript technologies like
-      React, Node or Vue but I love to try new stuff which occurs every day.
-    </div>
-    <div class="mt-4 text-xl">
-      I invite you to <NuxtLink to="/">get to know me</NuxtLink> a little better
-      and <NuxtLink to="/">reach me out</NuxtLink> to say hello.
+    <div class="mt-16 border-2 border-gray-500 p-8 rounded-md">
+      <div class="text-xl text-center">
+        I'm fullstack web engineer living in Central Europe. I'm taking part in
+        web development journey for many years and I can see how web is
+        transforming. I mostly work with stable Javascript technologies like
+        React, Node or Vue but I love to try new stuff which occurs every day.
+      </div>
+      <div class="mt-4 text-xl text-center">
+        I invite you to <NuxtLink to="/">get to know me</NuxtLink> a little
+        better and <NuxtLink to="/">reach me out</NuxtLink> to say hello.
+      </div>
     </div>
   </div>
 </template>
