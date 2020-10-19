a4616683e4489f4eca0878a46577c8c604c3b7f2
Patryk Gułaś <patryk.gulas@digifit.pl>
2020-10-19 15:43:01 +0200

    feat: favicon and meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,15 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'indefio',
+    title: 'Indefio - web engineering',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Indefio is a place about web development.',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
